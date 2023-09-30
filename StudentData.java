58c56a8e1f838c2cb4099044ebec76fb4b299e79
Harinder Kaur <harinder031998kaur@gmail.com>
2023-09-30 14:43:06 +1000

    Update StudentData.java

--- StudentData.java
+++ StudentData.java
@@ -2,4 +2,31 @@
 public class StudentData {
     public String firstName, lastName;
     public int studentId, a1, a2, a3, totalMarks;
+
+    //create constructor
+
+    public StudentData(String firstName, String lastName, int studentId, int a1, int a2, int a3)
+    {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.studentId = studentId;
+        this. a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+    }
+
+    public void getMinimumMarks()
+    {
+
+    }
+
+    public void getMaximumMarks()
+    {
+
+    }
+
+    public void printDetails()
+    {
+        
+    }
 }
