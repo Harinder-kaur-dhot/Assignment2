286b8e02ec9dfd96c60581d2d02191060a39a527
Harinder Kaur <harinder031998kaur@gmail.com>
2023-10-01 17:18:53 +1000

    Update FileMarksCalculator.java

--- FileMarksCalculator.java
+++ FileMarksCalculator.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 public class FileMarksCalculator {
     // Create main method
     public static void main(String[] arg) {
+        int i = 0, choice; // counter or each line in the file and choice for menu
+        boolean option = true;
         // declare a list to store data, the list is StudentData type
         List<StudentData> studentDetails = new ArrayList<>();
         // create exception handling
@@ -16,12 +18,11 @@ public class FileMarksCalculator {
             File myFile = new File("prog5001_students_grade_2022.txt");
             Scanner myReader = new Scanner(myFile); // scanner method to read file
             System.out.println("file found");
-            int i = 0, choice; // counter or each line in the file and choice for menu
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 i++;
                 if (i > 2) {
-                    if (data.isEmpty() || data.startsWith("\\"))
+                    if (data.isBlank() || data.isEmpty() || data.startsWith("/"))
                         continue;
                     // now we need to break the data in to chuncks or split it by ","
                     String[] chuncks = data.split(",");
@@ -40,46 +41,6 @@ public class FileMarksCalculator {
                 }
             }
             myReader.close();
-
-            // create menu
-
-            do {
-                System.out.println("Welcome");
-                System.out.println("Press 1 to print student deatils.");
-                System.out.println("Press 2 to get total marks.");
-                System.out.println("Press 3 to get top 5 student's details with marks.");
-                System.out.println("Press 4 to get the  Lowest scorer's details");
-                System.out.println("Press 5 to exit");
-                System.out.println("Please enter the valid option input");
-                Scanner getChoice = new Scanner(System.in);
-                choice = getChoice.nextInt();
-                switch (choice) {
-                    case 1: {
-                        System.out.println("Full Name   Student ID  A1  A2  A3  Total Marks");
-                        break;
-                    }
-
-                    case 2:
-
-                        break;
-
-                    case 3:
-
-                        break;
-
-                    case 4:
-
-                        break;
-
-                    case 5:
-                        System.out.print("Thank you");
-                        System.exit(0);
-                        getChoice.close();
-
-                    default:
-                        break;
-                }
-            } while (choice != 5);
         }
 
         catch (
@@ -87,5 +48,79 @@ public class FileMarksCalculator {
         FileNotFoundException e) {
             System.out.println("No file found");
         }
+
+        while (option == true) {
+            System.out.println("Welcome");
+            System.out.println("Press 1 to print student deatils in a list.");
+            System.out.println("Press 2 to print student deatils in a block.");
+            System.out.println("Press 3 to get total marks.");
+            System.out.println("Press 4 to get top 5 student's details with marks.");
+            System.out.println("Press 5 to get the  Lowest scorer's details");
+            System.out.println("Press 6 to exit");
+            System.out.println("Please enter the valid option input");
+            Scanner getChoice = new Scanner(System.in);
+            choice = getChoice.nextInt();
+            switch (choice) {
+                case 1: {
+                    for (i = 0; i < studentDetails.size(); i++) {
+                        StudentData student = studentDetails.get(i);
+                        System.out.println(
+                                student.getfullName() + "   " + student.getStudentId() + "   " + student.getA1()
+                                        + "   "
+                                        + student.getA2() + "   " + student.getA3() + "   "
+                                        + student.getTotalMarks());
+                    }
+                    break;
+                }
+
+                case 2: {
+                    for (i = 0; i < studentDetails.size(); i++) {
+                        StudentData student = studentDetails.get(i);
+                        System.out.println("Name: " + student.getfullName());
+                        System.out.println("Student ID: " + student.getStudentId());
+                        System.out.println("Assignment 1: " + student.getA1());
+                        System.out.println("Assignment 2: " + student.getA2());
+                        System.out.println("Assignment 3: " + student.getA3());
+                        System.out.println("Total Marks: " + student.getTotalMarks());
+                        System.out.println();
+                    }
+                    break;
+                }
+
+                case 3: {
+                    double threshold;
+                    System.out.println("Please enter the marks threshold.");
+                    Scanner sc = new Scanner(System.in);
+                    threshold = sc.nextDouble();
+                    for (i = 0; i < studentDetails.size(); i++) {
+                        StudentData student = studentDetails.get(i);
+                        if (student.getTotalMarks() < threshold) {
+                            System.out.println("Name: " + student.getfullName());
+                            System.out.println("Student ID: " + student.getStudentId());
+                            System.out.println("Total Marks: " + student.getTotalMarks());
+                            System.out.println();
+                        }
+                        sc.close();
+                    }
+                    break;
+                }
+                case 4: {
+                    // Lets first sort the list and then it will be esay to get top 5 and the lowest
+                    // 5
+                }
+
+                    break;
+
+                case 5: {
+                    System.out.print("Thank you");
+                    System.exit(0);
+                }
+                    getChoice.close();
+
+                default:
+                    break;
+            }
+        }
+
     }
 }
